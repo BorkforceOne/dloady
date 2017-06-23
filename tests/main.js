23bc00751681040a6ceae9d859d68a1699bc6208
Brandon Garling <bjg96@nau.edu>
2017-06-23 01:03:55 -0700

    Improve testing workflow by adding chai-as-promised

--- tests/main.js
+++ tests/main.js
@@ -1,10 +1,23 @@
-import assert from 'assert';
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
 
-describe('dloady', function() {
-  describe('#Load()', function() {
-    it('Should not die when loading', function() {
+chai.use(chaiAsPromised);
+
+// Then either:
+const expect = chai.expect;
+
+describe('dloady', () => {
+  describe('Loading', () => {
+    it('Should not die when loading', () => {
       const dloady = require('../src');
-      dloady.Load();
+      expect(dloady).not.to.be.null;
     });
+  });
+  describe('#Download()', () => {
+    it('Should download a remote file successfully', () => {
+      const dloady = require('../src');
+
+      return dloady.Download("http://ipv4.download.thinkbroadband.com/50MB.zip", "50MB.zip");
+    })
   });
 });
